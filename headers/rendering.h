ecc37c61fed1822ecc938ae777f7315aab7ce34e
Peyton <peytonthegreat0@gmail.com>
2026-09-04 18:18:57 -0400

    trying to get tiles to render

--- headers/rendering.h
+++ headers/rendering.h
@@ -12,4 +12,5 @@ extern int tileSizeX;
 extern int tileSizeY;
 
 void Update();
+void Display();
 void Initialize();
--- src/rendering.c
+++ src/rendering.c
@@ -1,6 +1,7 @@
 // headers //
 
 #include "../headers/rendering.h"
+#include <stdio.h>
 
 // variables //
 
@@ -12,11 +13,44 @@ int tileSizeY;
 
 // functions //
 
-void Update() //runs every frame
+void Display() //runs every frame
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    
+    // draw board //
+
+    // light tiles
+
+    glColor3ub(lightRGB.r, lightRGB.g, lightRGB.b);
+
+    glBegin(GL_QUADS);
+
+    for (int y = 0; y <= 7; y++)
+    {
+        for (int x = 0; x <= 3; x++)
+        {
+            int cX1 = tileSizeX * x;
+            int cY1 = tileSizeY * y;
+
+            int cX2 = tileSizeX * x + tileSizeX;
+            int cY2 = tileSizeY * y + tileSizeY;
+
+            glVertex2i(cX1, cY1);
+            glVertex2i(cX1, cY2);
+            glVertex2i(cX2, cY2);
+            glVertex2i(cX2, cY1);
+        }
+    } 
+
+    glEnd();
+
+    glutSwapBuffers();
+
+}
+
+void Update()
+{
+    glutPostRedisplay();
 }
 
 void Initialize() //runs at startup
@@ -31,6 +65,8 @@ void Resize(int newX, int newY) //runs when the window is resized
 
     tileSizeX = windowX / 8;
     tileSizeY = windowY / 8;
+
+    glutPostRedisplay();
 }
 
 // main //
@@ -40,12 +76,14 @@ int main(int argc, char *argv[])
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
     glutCreateWindow("chess.exe");
-    glutDisplayFunc(Update);
 
+    glutDisplayFunc(Display);
     glutReshapeFunc(Resize);
 
     Resize(500, 500);
     Initialize();
     
+    glutIdleFunc(Update);
+
     glutMainLoop();
 }
